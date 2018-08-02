8bdeeb374ac987bc7012bbe9269353f0d8b176e3
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-08-02 16:46:59 +0000

    Removed template literals

--- salesTax.js
+++ salesTax.js
@@ -29,22 +29,22 @@ function calculateSalesTax(salesData, taxRates) {
     var salesSumByProvince = 0;
     var salesTaxByProvince = 0;
     //adds current business name object to output if true
-    if (!output[`${salesData[i]['name']}`]) {
-      output[`${salesData[i]['name']}`] = {
+    if (!output[salesData[i].name]) {
+      output[salesData[i].name] = {
                                             totalSales: 0,
                                             totalTaxes: 0
                                           };
     }
     //calculates total sales by province
-    for (var j = 0; j < salesData[i]['sales'].length; j++) {
-      salesSumByProvince += salesData[i]['sales'][j];
+    for (var j = 0; j < salesData[i].sales.length; j++) {
+      salesSumByProvince += salesData[i].sales[j];
     }
     //adds sales sum to business by province
-    output[`${salesData[i]['name']}`]['totalSales'] += salesSumByProvince;
+    output[salesData[i].name].totalSales += salesSumByProvince;
     //calculates sales tax by province
-    salesTaxByProvince = calculateTax(salesSumByProvince, taxRates[`${salesData[i]['province']}`]);
+    salesTaxByProvince = calculateTax(salesSumByProvince, taxRates[salesData[i].province]);
     //adds sales tax by province to business name object
-    output[`${salesData[i]['name']}`]['totalTaxes'] += salesTaxByProvince;
+    output[salesData[i].name].totalTaxes += salesTaxByProvince;
   }
   return output;
 }
